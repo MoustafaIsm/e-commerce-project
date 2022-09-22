60c2c46068ead6fbaa959b12a6f8178dc54e0a0a
Moustafa <moustafaism013@hotmail.com>
2022-09-22 11:11:14 +0300

    EC-65 Added the variables needed in the navigation bar functionality

---
+++ client-frontend/scripts/scripts.js
@@ -0,0 +1,23 @@
+/* Variables */
+
+// Nav buttons
+const discoverNavBtn = document.getElementById("discover-nav-btn");
+const wishlistNavBtn = document.getElementById("wishlist-nav-btn");
+const cartNavBtn = document.getElementById("cart-nav-btn");
+const chatNavBtn = document.getElementById("chat-nav-btn");
+const profileNavBtn = document.getElementById("chat-nav-btn");
+
+// Pages
+const discoverPage = document.getElementById("discover-page");
+const wishlistPage = document.getElementById("wishlist-page");
+const cartPage = document.getElementById("cart-page");
+const chatPage = document.getElementById("chat-page");
+const profilePage = document.getElementById("profile-page");
+
+/* Eventlisteners */
+
+
+// Eventlisteners functions
+
+
+// Helper functions
