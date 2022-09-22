c880fe6df867353b00a8f05534f16603561d75b8
Moustafa <moustafaism013@hotmail.com>
2022-09-22 22:18:50 +0300

    EC-15 Fixed my huge problem when token verifyinh

--- ecommerce-server/client-apis/add-favorites-api.php
+++ ecommerce-server/client-apis/add-favorites-api.php
@@ -10,28 +10,13 @@
     $productId = $_GET["productId"];
     
     $token = $_GET["token"];
+    JWT::decode($token, new Key('fgh676', 'HS256'));
 
-    $key = 'fgh676';
-    $decoded = JWT::decode($token, new Key($key, 'HS256'));
-    $decoded_array = (array) $decoded;
+    $query = $mysqli->prepare("INSERT INTO `favorite_products`(`user_id`, `product_id`) VALUES (?,?)");
+    $query->bind_param("ii", $userId, $productId);
+    $result = $query->execute();
 
-    $userIdToken = $decoded_array["userId"];
-    $emailToken = $decoded_array["email"];
-
-    $query1 = $mysqli->prepare("SELECT * FROM `users` WHERE `user_id`=? AND `email`=?");
-    $query1->bind_param("is", $userIdToken, $emailToken);
-    $query1->execute();
-    $result1 = $query1->get_result();
-
-    if($result1->num_rows > 0) {
-        $query = $mysqli->prepare("INSERT INTO `favorite_products`(`user_id`, `product_id`) VALUES (?,?)");
-        $query->bind_param("ii", $userId, $productId);
-        $result = $query->execute();
-
-        $response["result"] = $result;
-    } else {
-        $response["result"] = "Invalid token";
-    }
+    $response["result"] = $result;
 
     echo json_encode($response);
 ?>
