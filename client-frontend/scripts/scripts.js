6acb82a1a9046791ea613f27a9f2c706d2014d66
Moustafa <moustafaism013@hotmail.com>
2022-09-22 11:23:59 +0300

    EC-65 Added the function for the discover navigation button functionality

--- client-frontend/scripts/scripts.js
+++ client-frontend/scripts/scripts.js
@@ -14,10 +14,36 @@ const cartPage = document.getElementById("cart-page");
 const chatPage = document.getElementById("chat-page");
 const profilePage = document.getElementById("profile-page");
 
+/* Eventlisteners functions */
+
+const openDiscoverPage = () => {
+    changeNavBtn("discover");
+    openPage("discover");
+}
+
 /* Eventlisteners */
 
+discoverNavBtn.addEventListener("click", openDiscoverPage);
 
-// Eventlisteners functions
+/* Helper functions */
 
+const changeNavBtn = (pageToOpen) => {
+    if (pageToOpen == "discover") {
+        discoverNavBtn.classList.add("nav-bar-item-active");
+        wishlistNavBtn.classList.remove("nav-bar-item-active");
+        cartNavBtn.classList.remove("nav-bar-item-active");
+        chatNavBtn.classList.remove("nav-bar-item-active");
+        profileNavBtn.classList.remove("nav-bar-item-active");
+    }
+}
 
-// Helper functions
+const openPage = (pageToOpen) => {
+    if (pageToOpen == "discover") {
+        discoverPage.classList.add("nav-bar-item-active");
+        wishlistPage.classList.remove("nav-bar-item-active");
+        cartPage.classList.remove("nav-bar-item-active");
+        chatPage.classList.remove("nav-bar-item-active");
+        profilePage.classList.remove("nav-bar-item-active");
+        console.log("donre");
+    }
+}
