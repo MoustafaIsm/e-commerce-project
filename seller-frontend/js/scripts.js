9b59845b306673bc5013ee9cf11e1aee0348815e
Ali Husseini <macbookpro@Apples-MacBook-Pro.local>
2022-09-23 11:55:42 +0300

    EC-78 declaring the pages and buttons in js

--- seller-frontend/js/scripts.js
+++ seller-frontend/js/scripts.js
@@ -1,3 +1,19 @@
+// Side bar navigation buttons
+const productNavBtn=document.getElementById("product-nav-btn")
+const categoryNavBtn=document.getElementById("category-nav-btn")
+const chatNavBtn=document.getElementById("chat-nav-btn")
+const discountNavBtn=document.getElementById("discount-nav-btn")
+const revenueNavBtn=document.getElementById("revenue-nav-btn")
+
+//Pages
+const productPage=document.getElementById("product-page")
+const categoryPage=document.getElementById("category-page")
+const chatPage =document.getElementById("chat-page")
+const discountPage=document.getElementById("discount-page")
+const revenuePage=document.getElementById("revenue-page")
+
+
+
 const productPopup = document.getElementById("popup-learn-more");
 const temp = document.getElementById("temp");
 temp.addEventListener("click", () =>{
