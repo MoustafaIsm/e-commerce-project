36f499a260d79422bd3a8b16633cf1217536b369
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-09-23 22:44:44 +0300

    EC-30 PHP: Updated get Total Number of Sellers API

--- ecommerce-server/admin-api/get-total-number-of-sellers-api.php
+++ ecommerce-server/admin-api/get-total-number-of-sellers-api.php
@@ -1,9 +1,26 @@
 <?php
 include('../connection.php');
+$period = $_GET['period'];
+$lastweek = date("Y-m-d",strtotime('- 7 days'));
+$lastmonth = date("Y-m-d",strtotime('- 30 days'));
+$lastyear = date("Y-m-d",strtotime('- 365 days'));
 
 // API to get total number of sellers
+if($period == 'week'){
+$stmt = $mysqli->prepare("SELECT COUNT(*) FROM `users` u WHERE role_id LIKE 3
+AND CAST(registered_at AS Date) > '$lastweek'");
+}
 
-$stmt = $mysqli->prepare(" SELECT COUNT(*) FROM `users` WHERE role_id LIKE 3");
+else if($period == 'month'){
+$stmt = $mysqli->prepare("SELECT COUNT(*) FROM `users` u WHERE role_id LIKE 3
+AND CAST(registered_at AS Date) > '$lastmonth'");
+}
+
+else {
+  $stmt = $mysqli->prepare("SELECT COUNT(*) FROM `users` u WHERE role_id LIKE 3
+  AND CAST(registered_at AS Date) > '$lastyear'");
+}
+
 if(!$stmt->execute()) {
     die("Error in create post");
 }
