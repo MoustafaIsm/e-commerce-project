442fa9d9473d275afacb2c59a09aacc818b1a1cc
Ali Husseini <macbookpro@Apples-MacBook-Pro.local>
2022-09-23 11:04:22 +0300

    EC-78 changing the layout of the popup form to dialog

---
+++ seller-frontend/js/scripts.js
@@ -0,0 +1,13 @@
+const productPopup = document.getElementById("popup-learn-more");
+const temp = document.getElementById("temp");
+temp.addEventListener("click", () =>{
+    productPopup.showModal();
+})
+
+
+
+const addProductPopup = document.getElementById("popup-add-more");
+const temp2 = document.getElementById("temp2");
+temp2.addEventListener("click", () =>{
+    addProductPopup.showModal();
+})
