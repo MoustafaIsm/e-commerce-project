a247ad3122f5c9c5d254089394a56ac421a4c309
Ali Husseini <macbookpro@Apples-MacBook-Pro.local>
2022-09-23 11:37:49 +0300

    EC-78 editing some css

--- seller-frontend/js/scripts.js
+++ seller-frontend/js/scripts.js
@@ -7,7 +7,7 @@ temp.addEventListener("click", () =>{
 
 
 const addProductPopup = document.getElementById("popup-add-more");
-const temp2 = document.getElementById("temp2");
+const temp2 = document.getElementById("add-product-button");
 temp2.addEventListener("click", () =>{
     addProductPopup.showModal();
 })
