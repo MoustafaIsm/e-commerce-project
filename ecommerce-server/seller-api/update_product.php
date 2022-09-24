7e3135ffd218c86053d4907ec1d0c1aaaf1c0ea9
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-09-25 00:08:32 +0300

    EC-91  updating the products by seller api

---
+++ ecommerce-server/seller-api/update_product.php
@@ -0,0 +1,30 @@
+<?php 
+
+include("../connection.php");
+
+require __DIR__ . '/vendor/autoload.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$token = $_POST["token"];
+JWT::decode($token, new Key('fgh676', 'HS256'));
+
+$seller_id = $_POST["seller_id"];
+$product_id = $_POST["product_id"];
+$prduct_name = $_POST["product_name"];
+$prduct_price = $_POST["product_price"];
+$description = $_POST["description"];
+$stock = $_POST["stock"];
+$added_at = $_POST["added_at"];
+$viewing_count = $_POST["viewing_count"];
+$product_picture = $_POST["product_picture"];
+
+$query = "UPDATE `products` SET  `product_name` = ? ,`product_price` = ? ,`description` = ? ,`stock` = ?,`added_at` = ? ,`viewing_count`= ? , `product_picture` = ? where products.product_id = ? and products.seller_id = ?";
+$query1 = $mysqli->prepare($query);
+$query1->bind_param("sisisiii", $prduct_name , $prduct_price , $description , $stock , $added_at , $viewing_count , $product_picture , $product_id , $seller_id);
+$query1->execute();
+
+$response1["success"] = true;
+echo json_encode($response1);
+
+?>
