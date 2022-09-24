5a009a789e9e5e78fb5c14b6dadee4aa0a6ccc1d
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-09-25 00:47:30 +0300

    EC-86 JS: Added Delete Seller Interactions

---
+++ admin-electron/js/delete-seller.js
@@ -0,0 +1,22 @@
+const deleteUrl = "http://localhost/e-commerce-project/ecommerce-server/admin-api/delete_seller.php";
+
+// function to fetch delete seller API
+const fetchdeleteSeller = (url, data) => {
+  console.log('fetch', data);
+  const resp =  axios.post(url, data);
+
+return resp;
+
+};
+
+const deleteSeller = (seller_id) => {
+  const data={
+    'token': token,
+    'user_id': seller_id
+  }
+const deleteResponse = fetchdeleteSeller(deleteUrl, data).then( response => {
+    console.log(response);
+});
+sellersCards.innerHTML = "";
+getSellers();
+};
