6e24d56e6a074cdb8734ff37016885e40ea53ee1
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-09-24 21:53:35 +0300

    EC-86 PHP: Updated get banned clients API

--- ecommerce-server/admin-api/get_baned_clients.php
+++ ecommerce-server/admin-api/get_baned_clients.php
@@ -1,13 +1,16 @@
-<?php 
+<?php
 
 include("../connection.php");
 
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers:*");
 require __DIR__ . '/vendor/autoload.php';
+$data = json_decode(file_get_contents("php://input"));
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
-$token = $_POST["token"];
+$token =  $data->token;
 JWT::decode($token, new Key('fgh676', 'HS256'));
 
 
