b22d98054b0e12644c4821af09bfae0c1bb7fa11
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-09-24 23:48:58 +0300

    EC-86 JS: Linked add sellers API

--- admin-electron/js/addseller.js
+++ admin-electron/js/addseller.js
@@ -1,5 +1,27 @@
 const saveseller = document.querySelector('.save-newseller');
 const error = document.querySelector('.error');
+const addsellerUrl = "http://localhost/e-commerce-project/ecommerce-server/admin-api/add_seller.php";
+
+hello();
+
+// function to fetch add sellers api
+const fetchaddSeller = (url, token) => {
+  const resp =  axios.post(url, token);
+
+return resp;
+
+};
+
+const addSeller = (data) => {
+
+const addResponse = fetchaddSeller(addsellerUrl, data).then((result) => {
+  console.log(result);
+});
+sellersCards.innerHTML = "";
+getSellers();
+
+};
+
 
 
 // Event Listeners
@@ -15,8 +37,27 @@ saveseller.addEventListener("click", () => {
   }
   else{
     error.classList.add('hidden');
-    //call the function that fetches api
-    //recall the function that gets sellers cards
+    const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+let currentDate = `${year}-${month}-${day}`;
+console.log(currentDate);
+    const data = {
+      'token':token,
+      'password': password,
+      'email': email,
+      'address': address,
+      'telephone' : phone,
+      'first_name' : fname,
+      'last_name' : lname,
+      'registered_at' : currentDate
+    }
+
+    addSeller(data);
+    newsellerSection.classList.add('hidden');
   }
 
 });
