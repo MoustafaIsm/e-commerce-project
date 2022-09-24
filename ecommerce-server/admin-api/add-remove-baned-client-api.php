c6063518922d728a56efcb2b4eb99d69424ab743
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-09-24 23:05:18 +0300

    EC-86 PHP: Updated add/remove ban API

--- ecommerce-server/admin-api/add-remove-baned-client-api.php
+++ ecommerce-server/admin-api/add-remove-baned-client-api.php
@@ -1,29 +1,32 @@
-<?php 
+<?php
 
 include("../connection.php");
 
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers:*");
 require __DIR__ . '/vendor/autoload.php';
+$data = json_decode(file_get_contents("php://input"));
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
-$user_id =$_POST["user_id"];
-$admin_id =$_POST["admin_id"];
+$user_id =$data->user_id;
+$admin_id =$data->admin_id;
 
-$token = $_POST["token"];
+$token = $data->token;
 JWT::decode($token, new Key('fgh676', 'HS256'));
 
 
-// get the info from user 
+// get the info from user
 $select_query = "Select * from bans where bans.user_id = ?";
 $query = $mysqli->prepare($select_query);
 $query->bind_param("i", $user_id);
 $query->execute();
 $array = $query->get_result();
-$result = $array->fetch_assoc(); 
+$result = $array->fetch_assoc();
 
 
-// check if the user is banned or not 
+// check if the user is banned or not
 if($result){
     $delete_query ="Delete from bans where bans.user_id = ?";
     $query1 = $mysqli->prepare($delete_query);
@@ -37,7 +40,10 @@ else {
     $query2 = $mysqli->prepare($insert_query);
     $query2->bind_param("ii",$admin_id, $user_id);
     if($query2->execute()){
-        echo "User was successfully banned.";
+        $response = [
+          'banned'=>'true'
+        ];
+        echo json_encode($response);
     };
 }
 
