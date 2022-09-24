78be4447b2dc3650d75234b0de112a986903bf78
Moustafa <moustafaism013@hotmail.com>
2022-09-24 23:28:27 +0300

    EC-105 liked the page too the signup api

--- client-frontend/scripts/index_script.js
+++ client-frontend/scripts/index_script.js
@@ -7,7 +7,8 @@ const fNameInput = document.getElementById("f-name-input");
 const lNameInput = document.getElementById("l-name-input");
 const phoneNumber = document.getElementById("phone-number");
 const emailInput = document.getElementById("email-input");
-const assword = document.getElementById("password-input");
+const password = document.getElementById("password-input");
+const errorOutput = document.getElementById("error-output");
 
 if (typeof signupPopup.showModal !== 'function') {
     signupPopup.hidden = true;
@@ -25,6 +26,28 @@ const closeSignupPopup = () => {
 }
 
 const signupUser = () => {
+    if (fNameInput.value != "" && lNameInput.value != "" &&
+        phoneNumber.value != "" && emailInput.value != "" && password.value != "") {
+        isValidEmail = validateEmail(emailInput.value);
+        if (isValidEmail) {
+            const formData = new FormData();
+            formData.append("password", password.value);
+            formData.append("first_name", fNameInput.value);
+            formData.append("last_name", lNameInput.value);
+            formData.append("email", emailInput.value);
+            formData.append("address", "NA");
+            formData.append("telephone", phoneNumber.value);
+            formData.append("profile_picture", "NA");
+            formData.append("registered_at", getCurrentDate);
+            axios.post("http://localhost/SEF/e-commerce-project/ecommerce-server/client-apis/add-user-api.php", formData)
+                .then((response) => { })
+                .catch((error) => console.log(error));
+            closeSignupPopup();
+        } else {
+            errorOutput.textContent = "Email invalid!";
+            errorOutput.style.color = "red";
+        }
+    }
 }
 
 
@@ -37,9 +60,19 @@ signupBtn.addEventListener("click", signupUser);
 
 // helper functions
 
-function ValidateEmail(mail) {
+const validateEmail = (mail) => {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
         return true;
     }
     return false;
+}
+
+const getCurrentDate = () => {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
 }
