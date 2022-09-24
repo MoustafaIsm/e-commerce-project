7c7fdf8f5f3a3970422f241cc57f5d5a29f06625
Ramzi El Ashkar <112473529+ramzielashkar@users.noreply.github.com>
2022-09-24 17:56:27 +0300

    Update get_baned_clients.php

--- ecommerce-server/admin-api/get_baned_clients.php
+++ ecommerce-server/admin-api/get_baned_clients.php
@@ -11,7 +11,11 @@ $token = $_POST["token"];
 JWT::decode($token, new Key('fgh676', 'HS256'));
 
 
-$query = "select u.user_id, u.first_name, u.last_name, u.email, u.address, u.profile_picture from users as u inner join bans as b on u.user_id = b.user_id where u.role_id = 2";
+$query = "select u.user_id, u.first_name, u.last_name, u.email, u.address, u.profile_picture,  SUM(c.total_cost) as total_purchases, COUNT(p.product_id) as total_items
+from users as u
+INNER JOIN carts c ON c.user_id like u.user_id
+INNER JOIN products_in_carts p ON p.cart_id = c.cart_id
+inner join bans as b on u.user_id = b.user_id where u.role_id = 2";
 $query1 = $mysqli->prepare($query);
 
 $query1->execute();
