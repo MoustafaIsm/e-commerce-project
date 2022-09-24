26d273861e2886959482bd010c0390711988b76b
Ramzi El Ashkar <112473529+ramzielashkar@users.noreply.github.com>
2022-09-24 17:48:00 +0300

    Update get_not_baned_clients.php

--- ecommerce-server/admin-api/get_not_baned_clients.php
+++ ecommerce-server/admin-api/get_not_baned_clients.php
@@ -11,7 +11,12 @@ $token = $_POST["token"];
 JWT::decode($token, new Key('fgh676', 'HS256'));
 
 
-$query = "select * from users as u where not exists (select user_id from bans as b where b.user_id = u.user_id) and u.role_id = 2";
+$query = "select DISTINCT u.*, SUM(c.total_cost) as total_purchases, COUNT(p.product_id) as total_items
+from users u
+INNER JOIN carts c ON c.user_id like u.user_id
+INNER JOIN products_in_carts p ON p.cart_id = c.cart_id
+where not exists (select user_id from bans as b where b.user_id = u.user_id) and u.role_id = 2
+";
 $query1 = $mysqli->prepare($query);
 
 $query1->execute();
