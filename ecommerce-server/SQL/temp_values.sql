11f21df189967c8347349cc92de77f83708a63fd
Moustafa <moustafaism013@hotmail.com>
2022-09-24 13:54:38 +0300

    EC-85 Added more temporary values for testing purposes

--- ecommerce-server/SQL/temp_values.sql
+++ ecommerce-server/SQL/temp_values.sql
@@ -1,20 +1,55 @@
 -- --------------------------------------------------------
+-- data for table `user_roles`
+
+INSERT INTO `user_roles` (`role_id`, `role_name`) 
+VALUES
+(1, 'admin'),
+(2, 'client'),
+(3, 'seller');
+
+-- --------------------------------------------------------
+-- data for table `users`
+
+INSERT INTO `users` (`password`, `first_name`, `last_name`, `email`, `address`, `telephone`, `profile_picture`, `role_id`, `registered_at`) VALUES
+('12345', 'mustafa', 'ismail', 'mustafa1@gmail.com', 'sour', 70123456, 'NA', 1, '2020-1-1'),
+('12345', 'Simon', 'ismail', 'simon@gmail.com', 'sour', 70125456, 'NA', 1, '2021-1-1'),
+('8976', 'mohamad', 'kayali', 'mohamad3@gmail.com', 'beirut', 70898765, 'NA', 2, '2021-3-25'),
+('111888', 'ahmad', 'moussa', 'ahamd3@gmail.com', 'jbeil', 70895321, 'NA', 2, '2022-9-1'),
+('999666', 'fatima', 'kabalan', 'fkabalan1@gmail.com', 'meis', 70456311, 'NA', 3, '2022-5-12'),
+('999666', 'Joe', 'kabalan', 'joe@gmail.com', 'meis', 70456311, 'NA', 3, '2022-7-12');
+
+-- --------------------------------------------------------
+-- data for table `products`
+
+INSERT INTO `products` (`product_name`, `product_price`, `description`, `stock`, `viewing_count`, `seller_id`, `added_at`) VALUES
+('Shampoo', 30, 'Best one yet.', 4, 1, 5, '2022-7-15'),
+('Hand soap', 10, 'Best one yet.', 10, 11, 6, '2022-8-15');
+
+-- --------------------------------------------------------
 -- data for table `ads`
 
-INSERT INTO `ads` (`add_id`, `product_id`, `ad_cost`) VALUES
-(1, 1, '100'),
-(2, 2, '70');
+INSERT INTO `ads` (`product_id`, `ad_cost`) VALUES
+(1, '100'),
+(2, '70');
+
 -- --------------------------------------------------------
 -- data for table `bans`
 
 INSERT INTO `bans` (`admin_id`, `user_id`) VALUES
-(1, 2),
-(1, 5);
+(1, 3);
+
 -- --------------------------------------------------------
 -- data for table `carts`
 
-INSERT INTO `carts` (`cart_id`, `product_id`, `user_id`, `purchase_date`) VALUES
-(1, 1, 2, '2022-09-17 16:13:09');
+INSERT INTO `carts` (`user_id`, `purchase_date`) VALUES
+(3, '2022-09-17 16:13:09');
+
+-- --------------------------------------------------------
+-- data for table `products_in_carts`
+
+INSERT INTO `products_in_carts` (`product_id`, `cart_id`) VALUES
+(1, 1),
+(2, 1);
 
 -- --------------------------------------------------------
 -- data for table `categories`
@@ -26,54 +61,35 @@ INSERT INTO `categories` (`category_id`, `category_name`, `seller_id`) VALUES
 -- data for table `chats`
 
 INSERT INTO `chats` (`client_user_id`, `seller_user_id`, `chat_text`, `date`) VALUES
-(2, 4, 'hiiiii', '2022-09-23'),
-(5, 4, 'hello', '2022-09-15');
+(3, 5, 'hiiiii', '2022-09-23'),
+(4, 5, 'hello', '2022-09-15');
 -- --------------------------------------------------------
 -- data for table `discounts`
 
-INSERT INTO `discounts` (`discount_id`, `active`, `percentage`, `seller_id`) VALUES
-(1, 1, 60, 4),
-(2, 0, 70, 4);
+INSERT INTO `discounts` (`active`, `percentage`, `seller_id`) VALUES
+(1, 60, 5),
+(0, 70, 5);
 -- --------------------------------------------------------
 -- data for table `favorite_products`
 
 INSERT INTO `favorite_products` (`user_id`, `product_id`) VALUES
-(2, 1),
-(5, 2);
--- --------------------------------------------------------
--- data for table `products`
+(3, 1),
+(3, 2);
 
-INSERT INTO `products` (`product_id`, `product_name`, `product_price`, `description`, `sponsered`, `stock`, `viewing_count`) VALUES
-(1, 'shampoo', '18', 'for dry hair','elvive', 6, 0),
-(2, 'perfum', '13', 'nice scent','far away', 7, 0);
 -- --------------------------------------------------------
 -- data for table `product_categories`
 
 INSERT INTO `product_categories` (`category_id`, `product_id`) VALUES
 (1, 1),
 (2, 2);
--- --------------------------------------------------------
--- data for table `users`
 
-INSERT INTO `users` (`user_id`, `password`, `first_name`, `last_name`, `email`, `address`, `telephone`, `profile_picture`, `role_id`) VALUES
-(1, '12345', 'mustafa', 'ismail', 'mustafa1@gmail.com', 'sour', 70123456, 'jpg', 1),
-(2, '8976', 'mohamad', 'kayali', 'mohamad3@gmail.com', 'beirut', 70898765, 'png', 2),
-(4, '999666', 'fatima', 'kabalan', 'fkabalan1@gmail.com', 'meis', 70456311, 'jpg', 3),
-(5, '111888', 'ahmad', 'moussa', 'ahamd3@gmail.com', 'jbeil', 70895321, 'jpg', 2);
--- --------------------------------------------------------
--- data for table `user_roles`
-
-INSERT INTO `user_roles` (`role_id`, `role_name`) VALUES
-(1, 'admin'),
-(2, 'client'),
-(3, 'seller');
 -- --------------------------------------------------------
 -- data for table `vouchers`
 
 INSERT INTO `vouchers` (`from_user_id`, `to_user_id`, `voucher_amount`) VALUES
-(2, 5, '100');
+(3, 4, '100');
 -- --------------------------------------------------------
 -- data for table `wishlists`
 
 INSERT INTO `wishlists` (`product_id`, `user_id`) VALUES
-(2, 2);
+(2, 4);
