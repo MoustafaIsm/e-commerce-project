3958dbc49356089a2fc8dc8c63800a8799df5d88
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-09-25 12:17:08 +0300

    EC-95 getting all discount codes of the seller api

---
+++ ecommerce-server/seller-api/get_discount_seller_codes_api.php
@@ -0,0 +1,29 @@
+<?php 
+
+include("../connection.php");
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$token = $_POST["token"];
+JWT::decode($token, new Key('fgh676', 'HS256'));
+
+
+$seller_id = $_POST["seller_id"];
+
+$query = "SELECT  *  FROM `discounts` WHERE discounts.seller_id = ? ORDER BY discounts.discount_id ASC";
+$query1 = $mysqli->prepare($query);
+$query1->bind_param("i", $seller_id);
+$query1->execute();
+$array = $query1->get_result();
+
+$response = [];
+
+while ($c = $array->fetch_assoc()) {
+    $response[] = $c;
+}
+echo json_encode($response);
+
+?>
