cca11d3072fd7b80ebfe08d89d9500c83c06b10e
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-09-25 15:11:13 +0300

    EC-86 PHP: Updated APIs

--- ecommerce-server/admin-api/get-total-number-of-clients-api.php
+++ ecommerce-server/admin-api/get-total-number-of-clients-api.php
@@ -1,23 +1,26 @@
 <?php
 include('../connection.php');
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers:*");
 require __DIR__ . '/vendor/autoload.php';
+$data = json_decode(file_get_contents("php://input"));
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
-$token = $_POST["token"];
+$token = $data->token;
 JWT::decode($token, new Key('fgh676', 'HS256'));
-$period = $_POST['period'];
+$period = $data->period;
 $lastweek = date("Y-m-d",strtotime('- 7 days'));
 $lastmonth = date("Y-m-d",strtotime('- 30 days'));
 $lastyear = date("Y-m-d",strtotime('- 365 days'));
 
 // API to get total number of clients
-if($period == 'week'){
+if($period == 'This Week'){
 $stmt = $mysqli->prepare("SELECT COUNT(*) FROM `users` u WHERE role_id LIKE 2
 AND CAST(registered_at AS Date) > '$lastweek'");
 }
 
-else if($period == 'month'){
+else if($period == 'This Month'){
 $stmt = $mysqli->prepare("SELECT COUNT(*) FROM `users` u WHERE role_id LIKE 2
 AND CAST(registered_at AS Date) > '$lastmonth'");
 }
